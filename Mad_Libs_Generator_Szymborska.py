bcff7e2681e7da2466dbc9578f1a00a985f1c9e7
martynadyja <48220304+martynadyja@users.noreply.github.com>
2020-09-20 18:02:50 +0200

    Add files via upload

---
+++ Mad_Libs_Generator_Szymborska.py
@@ -0,0 +1,24 @@
+#Mad Libs Game
+
+noun = input("Enter a noun_singular_denominator: ")
+verb = input("Enter a verb_singular_third person_present: ")
+noun2 = input("Enter a noun2_singular_denominator: ")
+noun3 = input("Enter a noun3_singular_instrumental: ")
+verb2 = input("Enter a verb2_singular_third person_neuter_future: ")
+verb3 = input("Enter a verb3_singular_third person_neuter_future: ")
+adjective = input("Enter a adjective_plural_locative_non-masculine: ")
+adjective2 = input("Enter a adjective2_plural_locative_masculine: ")
+noun4 = input("Enter a noun4_singular_accusative: ")
+
+print(" ")
+print("Niech " +noun + " tylko wróci,")
+print("Niech no się " +verb + ".")
+print("Już " +noun2 + " się dowie,")
+print("że tak z " +noun3 + " nie można.")
+print("Będzie się " + verb2 + " w jego stronę")
+print("jakby się wcale nie " + verb3 + ",")
+print("pomalutku,")
+print("na bardzo " +adjective + " łapach.")
+print("O " +adjective2 + " skoków pisków na " +noun4 + ".")
+
+
